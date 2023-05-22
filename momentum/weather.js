4329f4fde4919369ffe9373ee9409f3df72a22f1
geonno <ycygh@naver.com>
2023-05-22 17:52:52 +0900

    final

--- momentum/weather.js
+++ momentum/weather.js
@@ -1,8 +1,17 @@
+const api_keys = "b48084576d3c400c39e87f8012d83fce"
+
 function ongeook(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(`you live in ${lat} ${lng}`)
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_keys}`
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span : first-child")
+        const city = document.querySelector("#weather span : last-child")
+        city.innerText = data.name
+        weather.innerText = data.weather[0].main
+    })
 }
+
 
 function ongeoerror(){
     alert('cant find you. no weather for you')
